f1bf931b18e37baa09790b1517c1b6d4e0811f3b
mgruben <GrubenM@gmail.com>
2016-11-24 09:49:43 -0600

    Implement getter for triangular numbers

--- TriPentHex.cpp
+++ TriPentHex.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <math.h>
 
+using namespace std;
+
+long long triangle(int n) {
+    return ((long long)n * ((long long)n + 1)) / 2;
+}
+
+void test() {
+    cout << triangle(285);
+}
+
+int main() {
+    test();
+    return 0;
+}
