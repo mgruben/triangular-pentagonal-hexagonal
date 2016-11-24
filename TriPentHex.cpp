3023180ef993628b96765bccdb8160b39125b8f1
mgruben <GrubenM@gmail.com>
2016-11-24 10:22:03 -0600

    Implement alternative hexagonal solution
    In the Project Euler [forums](https://projecteuler.net/thread=45) (page 1, post 1),
    user `bishwa` noted that every hexagonal number is also a triangular number.
    
    Thus, we don't have to explicitly check triangle numbers; we are guaranteed
    that a hexagonal number is a also a triangular number.
    
    Accordingly, rather than iterating through the triangle numbers,
    and checking whether they are hexagonal and whether they are pentagonal,
    simply iterate through the hexagonal numbers, and check whether they
    are pentagonal.

--- TriPentHex.cpp
+++ TriPentHex.cpp
@@ -7,6 +7,10 @@ long long triangle(int n) {
     return ((long long)n * ((long long)n + 1)) / 2;
 }
 
+long long hexagon(int n) {
+    return (long long)n * ((long long)n*2 - 1);
+}
+
 bool isPent(long long num) {
     double n_est = (1 + sqrt(1+24*num)) / 6;
     return fmod(n_est, 1) == 0;
@@ -17,17 +21,19 @@ bool isHex(long long num) {
     return fmod(n_est, 1) == 0;
 }
 
-void test() {
-    long long x = triangle(285);
-    cout << x << endl;
-    cout << isPent(x) << endl;
-    cout << isHex(x) << endl;
-    cout << isPent(x-1) << endl;
-    cout << isHex(x-1) << endl;
+void solveHex() {
+    int n = 144;
+    while (!isPent(hexagon(n))) n++;
+    cout << "Hexagonal number_" << n << ": " << hexagon(n) << endl;
+}
+
+void solveTri() {
+    int n = 286;
+    while (!isPent(triangle(n)) || !isHex(triangle(n))) n++;
+    cout << "Triangular number_" << n << ": " << triangle(n) << endl;
 }
 
 int main() {
-    int n = 286;
-    while (!isPent(triangle(n)) || !isHex(triangle(n))) n++;
-    cout << n << ": " << triangle(n) << endl;
+    solveHex();
+    return 0;
 }
