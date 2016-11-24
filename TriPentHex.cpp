cc93c200dafeaf85c1cfce5cd38482f53bf2963c
mgruben <GrubenM@gmail.com>
2016-11-24 09:50:39 -0600

    Add isPent check
    Copied from Project Euler #44 solution

--- TriPentHex.cpp
+++ TriPentHex.cpp
@@ -7,6 +7,11 @@ long long triangle(int n) {
     return ((long long)n * ((long long)n + 1)) / 2;
 }
 
+bool isPent(long long num) {
+    double n_est = (1 + sqrt(1+24*num)) / 6;
+    return fmod(n_est, 1) == 0;
+}
+
 void test() {
     cout << triangle(285);
 }
